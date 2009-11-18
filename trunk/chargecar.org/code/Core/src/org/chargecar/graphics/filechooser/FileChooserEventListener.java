94046d839e2ab56f0f521197da7e9833e32e86d1
garthabrindoid <garthabrindoid@2cecc1ca-ac6e-11de-9815-ff96df1888ed>
2009-11-18 05:46:17 +0000

    MotionXGPSRawFileConverter now launches a GUI if no KMZ or raw file is provided on the command line (the jar it's also double-clickable).

---
+++ trunk/chargecar.org/code/Core/src/org/chargecar/graphics/filechooser/FileChooserEventListener.java
@@ -0,0 +1,9 @@
+package org.chargecar.graphics.filechooser;
+
+/**
+ * @author Chris Bartley (bartley@cmu.edu)
+ */
+public interface FileChooserEventListener
+   {
+   void handleSelectedFileChange();
+   }
