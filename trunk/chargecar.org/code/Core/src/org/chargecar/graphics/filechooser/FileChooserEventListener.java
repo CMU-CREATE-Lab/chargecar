0fae4f859c2b4a82258f704062bed7f1e372c99b
garthabrindoid <garthabrindoid@2cecc1ca-ac6e-11de-9815-ff96df1888ed>
2009-11-23 21:50:24 +0000

    deleting unused classes

--- trunk/chargecar.org/code/Core/src/org/chargecar/graphics/filechooser/FileChooserEventListener.java
+++
@@ -1,9 +0,0 @@
-package org.chargecar.graphics.filechooser;
-
-/**
- * @author Chris Bartley (bartley@cmu.edu)
- */
-public interface FileChooserEventListener
-   {
-   void handleSelectedFileChange();
-   }
