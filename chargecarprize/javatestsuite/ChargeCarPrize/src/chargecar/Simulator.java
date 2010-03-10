5b544a3f907d3b8660b22e390ce8c3f8640e9e35
astyler <astyler@2cecc1ca-ac6e-11de-9815-ff96df1888ed>
2010-03-10 09:23:56 +0000

    fleshed out simulator, added naivecap class, needs impl.

--- chargecarprize/javatestsuite/ChargeCarPrize/src/chargecar/Simulator.java
+++ chargecarprize/javatestsuite/ChargeCarPrize/src/chargecar/Simulator.java
@@ -3,6 +3,20 @@
  */
 package chargecar;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import chargecar.util.PointFeatures;
+import chargecar.util.PowerFlows;
+import chargecar.util.Trip;
+import chargecar.battery.BatteryModel;
+import chargecar.battery.NaiveBattery;
+import chargecar.capacitor.CapacitorModel;
+import chargecar.capacitor.NaiveCapacitor;
+import chargecar.policies.Policy;
+import chargecar.policies.PolicyFactory;
+
 /**
  * @author Alex Styler
  *
@@ -13,7 +27,37 @@ public class Simulator {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		BatteryModel battery;
+		CapacitorModel capacitor;
+		Policy noCapBaseline;
+		Policy naiveBaseline;
+		Policy userPolicy;
+		
+		List<Trip> tripsToTest = new ArrayList<Trip>();
+
+		
+		}
+		
+	List<BatteryModel> simulateTripsNaive(Policy policy, List<Trip> trips){
+		List<BatteryModel> tripBatteries = new ArrayList<BatteryModel>();
+		List<CapacitorModel> tripCapacitors = new ArrayList<CapacitorModel>();
+			for(Trip trip : trips){
+				BatteryModel battery = new NaiveBattery();
+				CapacitorModel cap = new NaiveCapacitor();
+				policy.beginTrip(trip.getFeatures());
+				
+				for(PointFeatures point : trip.getPoints()){
+					PowerFlows pf = policy.calculatePowerFlows(point);
+					//point.validate(pf);
+					battery.drawCurrent(0.0, 1.0);//btom+btoc
+					//cap.drawCurrent(0.0,1.0);//ctom-btoc
+				}
+				tripBatteries.add(battery);
+				tripCapacitors.add(cap);
+		}
+			return tripBatteries;//return both for visualizer
+		
+		
 	}
 
 }
---
+++ chargecarprize/javatestsuite/ChargeCarPrize/src/chargecar/capacitor/NaiveCapacitor.java
@@ -0,0 +1,5 @@
+package chargecar.capacitor;
+
+public class NaiveCapacitor implements CapacitorModel {
+
+}
