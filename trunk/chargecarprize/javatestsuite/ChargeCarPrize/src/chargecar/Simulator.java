a212af50e59420768e2dfbda2707521f9647d298
astyler <astyler@2cecc1ca-ac6e-11de-9815-ff96df1888ed>
2010-03-10 09:31:43 +0000

    better OOP, refactoring.
    considering Live Visualizers

--- trunk/chargecarprize/javatestsuite/ChargeCarPrize/src/chargecar/Simulator.java
+++ trunk/chargecarprize/javatestsuite/ChargeCarPrize/src/chargecar/Simulator.java
@@ -26,38 +26,42 @@ public class Simulator {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		BatteryModel battery;
-		CapacitorModel capacitor;
-		Policy noCapBaseline;
-		Policy naiveBaseline;
-		Policy userPolicy;
-		
+	public static void main(String[] args) {		
 		List<Trip> tripsToTest = new ArrayList<Trip>();
-
+		Policy noCapBaseline = PolicyFactory.getNoCapPolicy();
+		Policy naiveBaseline = PolicyFactory.getNaiveBufferPolicy();
+		Policy userPolicy = PolicyFactory.getUserPolicy();
+		userPolicy.loadState();
 		
+		simulateTripsNaive(userPolicy, tripsToTest);
+		simulateTripsNaive(noCapBaseline, tripsToTest);
+		simulateTripsNaive(naiveBaseline, tripsToTest);
+		
+		//grab Results, visualize
 		}
 		
-	List<BatteryModel> simulateTripsNaive(Policy policy, List<Trip> trips){
+	private static List<BatteryModel> simulateTripsNaive(Policy policy, List<Trip> trips){
 		List<BatteryModel> tripBatteries = new ArrayList<BatteryModel>();
-		List<CapacitorModel> tripCapacitors = new ArrayList<CapacitorModel>();
-			for(Trip trip : trips){
+		List<CapacitorModel> tripCapacitors = new ArrayList<CapacitorModel>();		
+		for(Trip trip : trips){
 				BatteryModel battery = new NaiveBattery();
-				CapacitorModel cap = new NaiveCapacitor();
-				policy.beginTrip(trip.getFeatures());
-				
-				for(PointFeatures point : trip.getPoints()){
-					PowerFlows pf = policy.calculatePowerFlows(point);
-					//point.validate(pf);
-					battery.drawCurrent(0.0, 1.0);//btom+btoc
-					//cap.drawCurrent(0.0,1.0);//ctom-btoc
-				}
+				CapacitorModel cap = new NaiveCapacitor();				
+				simulateTrip(policy, trip, battery, cap);				
 				tripBatteries.add(battery);
 				tripCapacitors.add(cap);
 		}
-			return tripBatteries;//return both for visualizer
-		
-		
+		return tripBatteries;//return both for visualizer		
+	}
+
+	private static void simulateTrip(Policy policy, Trip trip, BatteryModel battery, CapacitorModel cap) {
+		policy.beginTrip(trip.getFeatures());
+		for(PointFeatures point : trip.getPoints()){
+			PowerFlows pf = policy.calculatePowerFlows(point);
+			//point.validate(pf);
+			battery.drawCurrent(0.0, 1.0);//btom+btoc
+			//cap.drawCurrent(0.0,1.0);//ctom-btoc
+		}
+		policy.endTrip();
 	}
 
 }
