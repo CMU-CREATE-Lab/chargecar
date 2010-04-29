f2e916aeaab4f428686ab7bfa9304bb7a1538270
astyler <astyler@2cecc1ca-ac6e-11de-9815-ff96df1888ed>
2010-04-29 20:23:48 +0000

    gpx parsing finished, translated GPX times to Calendar objects for midnight rollover & traffic lookup.   PointFeatures fleshed out.

--- chargecarprize/javatestsuite/ChargeCarPrize/src/chargecar/util/PointFeatures.java
+++ chargecarprize/javatestsuite/ChargeCarPrize/src/chargecar/util/PointFeatures.java
@@ -1,5 +1,6 @@
 package chargecar.util;
 
+import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -9,20 +10,58 @@ import java.util.Date;
  * such as GPS coordinates, power demand, speed, etc...
  */
 public class PointFeatures {
-	private Double latitude;
-	private Double longitude;
-	private Double elevation;
-	private Double acceleration;
-	private Double speed;
-	private Double powerDemand;
-	private Double period;
-	public PointFeatures(){
-		
+	final private Double latitude;
+	final private Double longitude;
+	final private Double elevation;
+	final private Double acceleration;
+	final private Double speed;
+	final private Double powerDemand;
+	final private Double period;
+	final private Calendar time;
+
+	public PointFeatures(Double latitude, Double longitude, Double elevation,
+			Double acceleration, Double speed, Double powerDemand,
+			Double period, Calendar time) {
+		super();
+		this.latitude = latitude;
+		this.longitude = longitude;
+		this.elevation = elevation;
+		this.acceleration = acceleration;
+		this.speed = speed;
+		this.powerDemand = powerDemand;
+		this.period = period;
+		this.time = time;
 	}
-	public double getPowerDemand(){
-		return 0.0;
+	public Double getLatitude() {
+		return latitude;
 	}
-	public double getPeriod() {
-		return 1.0;
+
+	public Double getLongitude() {
+		return longitude;
 	}
+
+	public Double getElevation() {
+		return elevation;
+	}
+
+	public Double getAcceleration() {
+		return acceleration;
+	}
+
+	public Double getSpeed() {
+		return speed;
+	}
+
+	public Double getPowerDemand() {
+		return powerDemand;
+	}
+
+	public Double getPeriod() {
+		return period;
+	}
+
+	public Calendar getTime() {
+		return time;
+	}
+
 }
