20a8a143711f7d86f8629da6b50fbcb0ed920abf
astyler <astyler@2cecc1ca-ac6e-11de-9815-ff96df1888ed>
2010-06-08 20:51:46 +0000

    java package rename

---
+++ chargecarprize/javatestsuite/ChargeCarPrize/src/org/chargecar/prize/policies/SpeedPolicy.java
@@ -0,0 +1,94 @@
+package org.chargecar.prize.policies;
+
+import org.chargecar.prize.battery.BatteryModel;
+import org.chargecar.prize.util.PointFeatures;
+import org.chargecar.prize.util.PowerFlowException;
+import org.chargecar.prize.util.PowerFlows;
+import org.chargecar.prize.util.TripFeatures;
+
+/**
+ * DO NOT EDIT
+ * 
+ * An example policy that uses just the current speed of the vehicle to
+ * determine how much space to leave in the capacitor. If it is under that
+ * space, it trickles power into the capacitor if possible. Illustrates the
+ * capacitor current limitations, the difference between +/- current, and using
+ * the models to keep track of state.
+ * 
+ * @author Alex Styler
+ */
+
+public class SpeedPolicy implements Policy {
+    private BatteryModel modelCap;
+    private BatteryModel modelBatt;
+    private String name = "Speed Trickle Policy";
+    
+    public void beginTrip(TripFeatures tripFeatures, BatteryModel batteryClone,
+	    BatteryModel capacitorClone) {
+	modelCap = capacitorClone;
+	modelBatt = batteryClone;
+    }
+    
+    public PowerFlows calculatePowerFlows(PointFeatures pf) {
+	double wattsDemanded = pf.getPowerDemand();
+	int periodMS = pf.getPeriodMS();
+	double speed = pf.getSpeed();
+	// leave more room in cap for regen braking from higher speeds
+	double targetCharge = modelCap.getMaxCharge() - 1.7*speed;
+	double minCapCurrent = modelCap.getMinCurrent(periodMS);
+	double maxCapCurrent = modelCap.getMaxCurrent(periodMS);
+	double defaultTrickleRate = -8000.0;
+	double capToMotorWatts = 0.0;
+	double batteryToCapWatts = 0.0;
+	double batteryToMotorWatts = 0.0;
+	if (wattsDemanded < minCapCurrent) {
+	    // drawing more than the cap has
+	    // battery is already getting drawn, don't trickle cap
+	    capToMotorWatts = minCapCurrent;
+	    batteryToMotorWatts = wattsDemanded - capToMotorWatts;
+	    batteryToCapWatts = 0;
+	} else if (wattsDemanded > maxCapCurrent) {
+	    // overflowing cap with regen power
+	    // cap is full, no need to trickle.
+	    capToMotorWatts = maxCapCurrent;
+	    batteryToMotorWatts = wattsDemanded - capToMotorWatts;
+	    batteryToCapWatts = 0;
+	} else {
+	    // capacitor can handle the demand
+	    capToMotorWatts = wattsDemanded;
+	    batteryToMotorWatts = 0;
+	    if (modelCap.getCharge() < targetCharge) {
+		batteryToCapWatts = defaultTrickleRate;
+	    } else {
+		//batteryToCapWatts = -defaultTrickleRate;
+	    }
+	    if (capToMotorWatts - batteryToCapWatts > maxCapCurrent) {
+		batteryToCapWatts = capToMotorWatts - maxCapCurrent;
+	    } else if(capToMotorWatts - batteryToCapWatts < minCapCurrent){
+		batteryToCapWatts = capToMotorWatts - minCapCurrent;
+	    }
+	}
+	
+	try {
+	    modelCap.drawCurrent(capToMotorWatts - batteryToCapWatts, pf);
+	    modelBatt.drawCurrent(batteryToMotorWatts + batteryToCapWatts, pf);
+	} catch (PowerFlowException e) {
+	}
+	
+	return new PowerFlows(batteryToMotorWatts, capToMotorWatts,
+		batteryToCapWatts);
+    }
+    
+    public void endTrip() {
+	modelCap = null;
+	modelBatt = null;
+    }
+    
+    public void loadState() {
+	// nothing to do
+    }
+    
+    public String getName() {
+	return name;
+    }
+}
---
+++ chargecarprize/javatestsuite/ChargeCarPrize/src/org/chargecar/prize/util/SimulationResults.java
@@ -0,0 +1,53 @@
+package org.chargecar.prize.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.chargecar.prize.battery.BatteryModel;
+
+
+/**
+ * DO NOT EDIT Stores the result of running a simulation over multiple trips for
+ * a single policy. At the moment, only stores the batteryCurrent integral for
+ * each trip, as that is what we're interested in with the simple batteries.
+ * 
+ * @author Alex Styler
+ * 
+ */
+public class SimulationResults {
+    private List<Double> batteryCurrentSquaredIntegrals;
+    private List<Double> chargeSpent;
+    private List<String> tripStrings;
+    private String policyName;
+    
+    public SimulationResults(String policyName) {
+	this.policyName = policyName;
+	tripStrings = new ArrayList<String>();
+	batteryCurrentSquaredIntegrals = new ArrayList<Double>();
+	chargeSpent = new ArrayList<Double>();
+    }
+    
+    public void addTrip(Trip trip, BatteryModel battery, BatteryModel capacitor) {
+	batteryCurrentSquaredIntegrals.add(battery.currentSquaredIntegral());
+	chargeSpent.add(battery.getMaxCharge() - battery.getCharge()
+		- capacitor.getCharge());
+	tripStrings.add(trip.toString());
+    }
+    
+    public List<Double> getBatteryCurrentSquaredIntegrals() {
+	return batteryCurrentSquaredIntegrals;
+    }
+    
+    public List<Double> getChargeSpent() {
+	return chargeSpent;
+    }
+    
+    public List<String> getTripStrings() {
+	return tripStrings;
+    }
+    
+    public String getPolicyName() {
+	return policyName;
+    }
+    
+}
