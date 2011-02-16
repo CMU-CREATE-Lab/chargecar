ff5c036f3d6696b143a7d210a2aa07b481786f77
garthabrindoid <garthabrindoid@2cecc1ca-ac6e-11de-9815-ff96df1888ed>
2011-02-16 18:40:35 +0000

    Added RepeatingActionCharacterDisplayMenuItemAction, modified the 5 existing menu item actions which did repeated actions to be subclasses, and did some other minor cleanup.

--- car/code/LCDDisplay/src/org/chargecar/lcddisplay/lcd/menu/action/CurrentsMenuItemAction.java
+++ car/code/LCDDisplay/src/org/chargecar/lcddisplay/lcd/menu/action/CurrentsMenuItemAction.java
@@ -1,74 +1,55 @@
 package org.chargecar.lcddisplay.lcd.menu.action;
 
+import java.util.concurrent.TimeUnit;
 import edu.cmu.ri.createlab.LCD;
 import edu.cmu.ri.createlab.LCDConstants;
 import edu.cmu.ri.createlab.LCDProxy;
 import edu.cmu.ri.createlab.display.character.CharacterDisplay;
-import edu.cmu.ri.createlab.display.character.menu.CharacterDisplayMenuItemAction;
+import edu.cmu.ri.createlab.display.character.menu.RepeatingActionCharacterDisplayMenuItemAction;
 import edu.cmu.ri.createlab.menu.MenuItem;
 import edu.cmu.ri.createlab.menu.MenuStatusManager;
-import edu.cmu.ri.createlab.util.thread.DaemonThreadFactory;
 import org.apache.log4j.Logger;
 import org.chargecar.lcddisplay.lcd.SensorBoard;
-
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.TimeUnit;
 
 /**
  * @author Paul Dille (pdille@andrew.cmu.edu)
  */
-public final class CurrentsMenuItemAction extends CharacterDisplayMenuItemAction {
-    private static final Logger LOG = Logger.getLogger(CurrentsMenuItemAction.class);
+public final class CurrentsMenuItemAction extends RepeatingActionCharacterDisplayMenuItemAction
+   {
+   private static final Logger LOG = Logger.getLogger(CurrentsMenuItemAction.class);
 
-    public CurrentsMenuItemAction(final MenuItem menuItem,
-                                  final MenuStatusManager menuStatusManager,
-                                  final CharacterDisplay characterDisplay) {
-        super(menuItem, menuStatusManager, characterDisplay);
-    }
+   public CurrentsMenuItemAction(final MenuItem menuItem,
+                                 final MenuStatusManager menuStatusManager,
+                                 final CharacterDisplay characterDisplay)
+      {
+      super(menuItem, menuStatusManager, characterDisplay, 0, 200, TimeUnit.MILLISECONDS);
+      }
 
-    private final ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor(new DaemonThreadFactory("VoltagesMenuItemAction.executor"));
-    private ScheduledFuture<?> scheduledFuture = null;
-    final LCD lcd = LCDProxy.getInstance();
-    final SensorBoard sensorboard = SensorBoard.getInstance();
+   @Override
+   protected void performAction()
+      {
+      final LCD lcd = LCDProxy.getInstance();
+      final SensorBoard sensorboard = SensorBoard.getInstance();
+      if (sensorboard == null || sensorboard.getBmsAndEnergy() == null)
+         {
+         LOG.debug("CurrentsMenuItemAction.run(): bms is null");
+         getCharacterDisplay().setLine(0, "No connection to BMS.");
+         getCharacterDisplay().setCharacter(LCDConstants.NUM_ROWS - 1, 0, " ");
+         return;
+         }
+      else if (lcd == null)
+         {
+         getCharacterDisplay().setLine(0, "No connection to LCD.");
+         getCharacterDisplay().setCharacter(LCDConstants.NUM_ROWS - 1, 0, " ");
+         return;
+         }
+      final double loadCurrent = Math.round(sensorboard.getBmsAndEnergy().getBmsState().getLoadCurrentAmps() * 100.0) / 100.0;
+      final double sourceCurrent = Math.round(sensorboard.getBmsAndEnergy().getBmsState().getSourceCurrentAmps() * 100.0) / 100.0;
 
-    public void activate() {
-        try {
-            scheduledFuture = executor.scheduleWithFixedDelay(new Runnable() {
-                @Override
-                public void run() {
-                    if (sensorboard == null || sensorboard.getBmsAndEnergy() == null) {
-                        LOG.debug("CurrentsMenuItemAction.run(): bms is null");
-                        getCharacterDisplay().setLine(0, "No connection to BMS.");
-                        getCharacterDisplay().setCharacter(LCDConstants.NUM_ROWS - 1, 0, " ");
-                        return;
-                    } else if (lcd == null) {
-                        getCharacterDisplay().setLine(0, "No connection to LCD.");
-                        getCharacterDisplay().setCharacter(LCDConstants.NUM_ROWS - 1, 0, " ");
-                        return;
-                    }
-                    final double loadCurrent = Math.round(sensorboard.getBmsAndEnergy().getBmsState().getLoadCurrentAmps() * 100.0) / 100.0;
-                    final double sourceCurrent = Math.round(sensorboard.getBmsAndEnergy().getBmsState().getSourceCurrentAmps() * 100.0) / 100.0;
-
-                    LOG.debug("CurrentsMenuItemAction.activate(): updating voltages");
-                    getCharacterDisplay().setLine(0, "Load Current: " + loadCurrent);
-                    getCharacterDisplay().setLine(1, "Source Current: " + sourceCurrent);
-                    getCharacterDisplay().setCharacter(LCDConstants.NUM_ROWS - 1, 0, " ");
-                }
-            }, 0, 200, TimeUnit.MILLISECONDS);
-        } catch (Exception e) {
-            // TODO: make this error message better
-            LOG.error("CurrentsMenuItemAction.activate(): failed to schedule task", e);
-        }
-    }
-
-    @Override
-    public void deactivate() {
-        if (scheduledFuture != null) {
-            LOG.debug("CurrentsMenuItemAction.deactivate(): cancelling task");
-            scheduledFuture.cancel(true);
-        }
-    }
-}
+      LOG.debug("CurrentsMenuItemAction.activate(): updating voltages");
+      getCharacterDisplay().setLine(0, "Load Current: " + loadCurrent);
+      getCharacterDisplay().setLine(1, "Source Current: " + sourceCurrent);
+      getCharacterDisplay().setCharacter(LCDConstants.NUM_ROWS - 1, 0, " ");
+      }
+   }
 
--- car/code/LCDDisplay/src/org/chargecar/lcddisplay/lcd/menu/action/VoltagesMenuItemAction.java
+++ car/code/LCDDisplay/src/org/chargecar/lcddisplay/lcd/menu/action/VoltagesMenuItemAction.java
@@ -1,75 +1,56 @@
 package org.chargecar.lcddisplay.lcd.menu.action;
 
+import java.util.concurrent.TimeUnit;
 import edu.cmu.ri.createlab.LCD;
 import edu.cmu.ri.createlab.LCDConstants;
 import edu.cmu.ri.createlab.LCDProxy;
 import edu.cmu.ri.createlab.display.character.CharacterDisplay;
-import edu.cmu.ri.createlab.display.character.menu.CharacterDisplayMenuItemAction;
+import edu.cmu.ri.createlab.display.character.menu.RepeatingActionCharacterDisplayMenuItemAction;
 import edu.cmu.ri.createlab.menu.MenuItem;
 import edu.cmu.ri.createlab.menu.MenuStatusManager;
-import edu.cmu.ri.createlab.util.thread.DaemonThreadFactory;
 import org.apache.log4j.Logger;
 import org.chargecar.lcddisplay.lcd.SensorBoard;
-
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.TimeUnit;
 
 /**
  * @author Paul Dille (pdille@andrew.cmu.edu)
  */
-public final class VoltagesMenuItemAction extends CharacterDisplayMenuItemAction {
-    private static final Logger LOG = Logger.getLogger(VoltagesMenuItemAction.class);
+public final class VoltagesMenuItemAction extends RepeatingActionCharacterDisplayMenuItemAction
+   {
+   private static final Logger LOG = Logger.getLogger(VoltagesMenuItemAction.class);
 
-    public VoltagesMenuItemAction(final MenuItem menuItem,
-                                  final MenuStatusManager menuStatusManager,
-                                  final CharacterDisplay characterDisplay) {
-        super(menuItem, menuStatusManager, characterDisplay);
-    }
+   public VoltagesMenuItemAction(final MenuItem menuItem,
+                                 final MenuStatusManager menuStatusManager,
+                                 final CharacterDisplay characterDisplay)
+      {
+      super(menuItem, menuStatusManager, characterDisplay, 0, 200, TimeUnit.MILLISECONDS);
+      }
 
-    private final ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor(new DaemonThreadFactory("VoltagesMenuItemAction.executor"));
-    private ScheduledFuture<?> scheduledFuture = null;
-    final LCD lcd = LCDProxy.getInstance();
-    final SensorBoard sensorboard = SensorBoard.getInstance();
+   @Override
+   protected void performAction()
+      {
+      final LCD lcd = LCDProxy.getInstance();
+      final SensorBoard sensorboard = SensorBoard.getInstance();
+      if (sensorboard == null || sensorboard.getBmsAndEnergy() == null)
+         {
+         LOG.debug("VoltagesMenuItemAction.run(): bms is null");
+         getCharacterDisplay().setLine(0, "No connection to BMS.");
+         getCharacterDisplay().setCharacter(LCDConstants.NUM_ROWS - 1, 0, " ");
+         return;
+         }
+      else if (lcd == null)
+         {
+         getCharacterDisplay().setLine(0, "No connection to LCD.");
+         getCharacterDisplay().setCharacter(LCDConstants.NUM_ROWS - 1, 0, " ");
+         return;
+         }
+      final double minVoltage = Math.round(sensorboard.getBmsAndEnergy().getBmsState().getMinimumCellVoltage() * 100.0) / 100.0;
+      final double maxVoltage = Math.round(sensorboard.getBmsAndEnergy().getBmsState().getMaximumCellVoltage() * 100.0) / 100.0;
+      final double averageVoltage = Math.round(sensorboard.getBmsAndEnergy().getBmsState().getAverageCellVoltage() * 100.0) / 100.0;
 
-    public void activate() {
-        try {
-            scheduledFuture = executor.scheduleWithFixedDelay(new Runnable() {
-                @Override
-                public void run() {
-                    if (sensorboard == null || sensorboard.getBmsAndEnergy() == null) {
-                        LOG.debug("VoltagesMenuItemAction.run(): bms is null");
-                        getCharacterDisplay().setLine(0, "No connection to BMS.");
-                        getCharacterDisplay().setCharacter(LCDConstants.NUM_ROWS-1,0," ");
-                        return;
-                    }else if (lcd == null) {
-                        getCharacterDisplay().setLine(0, "No connection to LCD.");
-                        getCharacterDisplay().setCharacter(LCDConstants.NUM_ROWS-1,0," ");
-                        return;
-                    }
-                    final double minVoltage = Math.round(sensorboard.getBmsAndEnergy().getBmsState().getMinimumCellVoltage() * 100.0) / 100.0;
-                    final double maxVoltage = Math.round(sensorboard.getBmsAndEnergy().getBmsState().getMaximumCellVoltage() * 100.0) / 100.0;
-                    final double averageVoltage = Math.round(sensorboard.getBmsAndEnergy().getBmsState().getAverageCellVoltage() * 100.0) / 100.0;
-
-                    LOG.debug("VoltagesMenuItemAction.activate(): updating voltages");
-                    getCharacterDisplay().setLine(0, "Min Voltage: " + minVoltage);
-                    getCharacterDisplay().setLine(1, "Max Voltage: " + maxVoltage);
-                    getCharacterDisplay().setLine(2, "Average Voltage: " + averageVoltage);
-                    getCharacterDisplay().setCharacter(LCDConstants.NUM_ROWS-1,0," ");
-                }
-            }, 0, 200, TimeUnit.MILLISECONDS);
-        } catch (Exception e) {
-            // TODO: make this error message better
-            LOG.error("VoltagesMenuItemAction.activate(): failed to schedule task", e);
-        }
-    }
-
-    @Override
-    public void deactivate() {
-        if (scheduledFuture != null) {
-            LOG.debug("VoltagesMenuItemAction.deactivate(): cancelling task");
-            scheduledFuture.cancel(true);
-        }
-    }
-}
+      LOG.debug("VoltagesMenuItemAction.activate(): updating voltages");
+      getCharacterDisplay().setLine(0, "Min Voltage: " + minVoltage);
+      getCharacterDisplay().setLine(1, "Max Voltage: " + maxVoltage);
+      getCharacterDisplay().setLine(2, "Average Voltage: " + averageVoltage);
+      getCharacterDisplay().setCharacter(LCDConstants.NUM_ROWS - 1, 0, " ");
+      }
+   }
