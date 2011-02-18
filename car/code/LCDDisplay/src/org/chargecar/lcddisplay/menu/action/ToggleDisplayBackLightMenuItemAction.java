432d0d539945713953e5577f7a1b828d9ad257bd
garthabrindoid <garthabrindoid@2cecc1ca-ac6e-11de-9815-ff96df1888ed>
2011-02-18 20:09:41 +0000

    The LCD instance used by these classes is now just a local variable rather than a class member.  This enables the menu to be created without the call to LCDProxy.getInstance() being invoked, which is nice for when you just want to create the menu but not actually cause a connection to the LCD to be initiated (e.g. for testing).

--- car/code/LCDDisplay/src/org/chargecar/lcddisplay/menu/action/ToggleDisplayBackLightMenuItemAction.java
+++ car/code/LCDDisplay/src/org/chargecar/lcddisplay/menu/action/ToggleDisplayBackLightMenuItemAction.java
@@ -1,5 +1,6 @@
 package org.chargecar.lcddisplay.menu.action;
 
+import java.util.Map;
 import edu.cmu.ri.createlab.display.character.CharacterDisplay;
 import edu.cmu.ri.createlab.display.character.menu.TwoOptionMenuItemAction;
 import edu.cmu.ri.createlab.menu.MenuItem;
@@ -9,14 +10,11 @@ import org.chargecar.lcddisplay.LCD;
 import org.chargecar.lcddisplay.LCDConstants;
 import org.chargecar.lcddisplay.LCDProxy;
 
-import java.util.Map;
-
 /**
  * @author Paul Dille (pdille@andrew.cmu.edu)
  */
 public final class ToggleDisplayBackLightMenuItemAction extends TwoOptionMenuItemAction {
     private static final Logger LOG = Logger.getLogger(ToggleDisplayBackLightMenuItemAction.class);
-    private final LCD lcd = LCDProxy.getInstance();
     private boolean isDisplayBackLightEnabled = true; //backlight is already turned on when the program starts
 
     public ToggleDisplayBackLightMenuItemAction(final MenuItem menuItem,
@@ -45,6 +43,7 @@ public final class ToggleDisplayBackLightMenuItemAction extends TwoOptionMenuIte
     }
 
     private void setDisplayBackLightEnabled(final boolean actionState) {
+        final LCD lcd = LCDProxy.getInstance();
         if (lcd == null) {
             LOG.error("ToggleDisplayBackLightMenuItemAction.setDisplayBackLightEnabled(): lcd is null");
             return;
