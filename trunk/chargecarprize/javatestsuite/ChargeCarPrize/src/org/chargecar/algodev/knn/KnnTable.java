705e0a733793176f6d64bab62ef42447e1a99b93
astyler <astyler@2cecc1ca-ac6e-11de-9815-ff96df1888ed>
2011-04-26 18:23:49 +0000

    Missing file

---
+++ trunk/chargecarprize/javatestsuite/ChargeCarPrize/src/org/chargecar/algodev/knn/KnnTable.java
@@ -0,0 +1,46 @@
+package org.chargecar.algodev.knn;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import org.chargecar.prize.util.PointFeatures;
+
+public class KnnTable implements Serializable{
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    private final List<KnnPoint> points;
+    private final List<Double> powers;
+    
+    public KnnTable(){
+	points = new ArrayList<KnnPoint>();
+	powers = new ArrayList<Double>();
+    }
+    
+    public KnnTable(List<KnnPoint> inpPoints, List<Double> inpPowers){
+	this.points = new ArrayList<KnnPoint>(inpPoints);
+	this.powers = new ArrayList<Double>(inpPowers);	
+    }
+    
+    public void addPoint(PointFeatures pf, double powerDemand){
+	powers.add(powerDemand);
+	int index = powers.size()-1;
+	points.add(new KnnPoint(pf,index));	
+    }
+    
+    public List<KnnPoint> getKnnPoints(){
+	return points;
+    }
+    
+    public List<Double> getPowers(){
+	return powers;
+    }
+    
+    public void endTrip(){
+	powers.add(null);
+    }
+    
+    
+    
+}
