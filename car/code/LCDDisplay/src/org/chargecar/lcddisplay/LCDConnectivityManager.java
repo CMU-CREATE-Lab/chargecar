36c24783de5e81bd3606efafd0d388fa47431528
garthabrindoid@gmail.com <garthabrindoid@gmail.com@2cecc1ca-ac6e-11de-9815-ff96df1888ed>
2011-06-01 19:23:43 +0000

    update due to changes in CREATE Lab Commons Device

--- car/code/LCDDisplay/src/org/chargecar/lcddisplay/LCDConnectivityManager.java
+++ car/code/LCDDisplay/src/org/chargecar/lcddisplay/LCDConnectivityManager.java
@@ -1,16 +1,15 @@
 package org.chargecar.lcddisplay;
 
-import edu.cmu.ri.createlab.device.CreateLabDeviceProxy;
 import edu.cmu.ri.createlab.device.connectivity.BaseCreateLabDeviceConnectivityManager;
 import org.apache.log4j.Logger;
 
 /**
  * @author Chris Bartley (bartley@cmu.edu)
  */
-final class LCDConnectivityManager extends BaseCreateLabDeviceConnectivityManager {
+final class LCDConnectivityManager extends BaseCreateLabDeviceConnectivityManager<LCD> {
     private static final Logger LOG = Logger.getLogger(LCDConnectivityManager.class);
 
-    protected CreateLabDeviceProxy scanForDeviceAndCreateProxy() {
+    protected LCD scanForDeviceAndCreateProxy() {
         LOG.debug("LCDConnectivityManager.scanForDeviceAndCreateProxy()");
 
         // If the user specified one or more serial ports, then just start trying to connect to it/them.  Otherwise,
@@ -33,7 +32,7 @@ final class LCDConnectivityManager extends BaseCreateLabDeviceConnectivityManage
                     LOG.debug("LCDConnectivityManager.scanForDeviceAndCreateProxy(): checking serial port [" + portName + "]");
                 }
 
-                final CreateLabDeviceProxy proxy = LCDProxy.create(portName);
+                final LCD proxy = LCDProxy.create(portName);
 
                 if (proxy == null) {
                     LOG.debug("LCDConnectivityManager.scanForDeviceAndCreateProxy(): connection failed, maybe it's not the device we're looking for?");
